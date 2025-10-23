e8232009dc3f6cea6877cca6692494f36d2d339d
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2025-10-23 18:49:19 -0400

    Time: 239 ms (43.88%) | Memory: 27.5 MB (94.17%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -5,6 +5,32 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
+        island = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    island += 1
+                    grid[i][j] = '0'
+                    queue = deque([(i,j)])
+
+                    while queue:
+                        r,c = queue.popleft()
+                        direction = [(0,1),(0,-1),(1,0),(-1,0)]
+                        for dr, dc in direction:
+                            nr,nc = r+dr, c+dc
+                            if 0<= nr < len(grid) and 0<= nc < len(grid[0]):
+                                if grid[nr][nc] == '1':
+                                    grid[nr][nc] = '0'
+                                    queue.append((nr,nc))
+
+        return island
+                
+
+                    
+        
+
+        
         # visited = set()
         # island = 0
 
@@ -26,26 +52,26 @@ class Solution(object):
         
         # return island
 
-        island = 0
+        # island = 0
         
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == '1':
-                    island += 1
-                    grid[r][c] = '0'
-                    queue = deque([(r,c)])
+        # for r in range(len(grid)):
+        #     for c in range(len(grid[0])):
+        #         if grid[r][c] == '1':
+        #             island += 1
+        #             grid[r][c] = '0'
+        #             queue = deque([(r,c)])
                 
-                    while queue:
-                        cr,cc = queue.popleft()
-                        direction = [(0,1),(0,-1), (1,0), (-1,0)]
-                        for dr, dc in direction:
-                            nr , nc = cr+dr, cc+dc
-                            if 0<= nr < len(grid) and 0<= nc < len(grid[0]):
-                                if grid[nr][nc] == '1':
-                                    grid[nr][nc] = '0'
-                                    queue.append((nr,nc))
+        #             while queue:
+        #                 cr,cc = queue.popleft()
+        #                 direction = [(0,1),(0,-1), (1,0), (-1,0)]
+        #                 for dr, dc in direction:
+        #                     nr , nc = cr+dr, cc+dc
+        #                     if 0<= nr < len(grid) and 0<= nc < len(grid[0]):
+        #                         if grid[nr][nc] == '1':
+        #                             grid[nr][nc] = '0'
+        #                             queue.append((nr,nc))
             
-        return island
+        # return island
                     
                 
                     
