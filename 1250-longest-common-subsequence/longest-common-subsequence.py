7bc7093b088231e01a61e3166bc175a1fe922de8
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2025-10-23 18:03:25 -0400

    Time: 603 ms (33.49%) | Memory: 34.6 MB (20.21%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -10,12 +10,12 @@ class Solution(object):
         n = len(text2)
         
         c = [[0 for i in range(n+1)] for j in range(m+1)]
-        for i in range(m):
-            for j in range(n):
-                if (text1[i] == text2[j]):
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if (text1[i-1] == text2[j-1]):
                     c[i][j] = c[i-1][j-1] + 1
                 else:
                     c[i][j] = max(c[i-1][j], c[i][j-1])
         
-        return c[m-1][n-1]
+        return c[m][n]
         
