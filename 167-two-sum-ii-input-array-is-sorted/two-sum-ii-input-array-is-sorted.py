eded3f1b3bc39da958892d295f15748772769bae
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2025-11-24 16:54:50 -0500

    Time: 3 ms (86.65%) | Memory: 13 MB (10.46%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -5,18 +5,24 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        left = 0
-        right = len(numbers) -1
+        # left = 0
+        # right = len(numbers) -1
 
-        while left < right:
-            s= numbers[left] + numbers[right]
-            if s == target:
-                return [left+1, right+1]
+        # while left < right:
+        #     s= numbers[left] + numbers[right]
+        #     if s == target:
+        #         return [left+1, right+1]
             
-            elif s < target:
-                left += 1
-            else:
-                right -= 1
+        #     elif s < target:
+        #         left += 1
+        #     else:
+        #         right -= 1
+        dicts={}
+
+        for i, num in enumerate(numbers):
+            if target - num in dicts:
+                return [ dicts[target-num]+1, i+1]
+            dicts[num] = i
      
 
                 
