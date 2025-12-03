c5524a319942945ed6c3c0e8d936e32f82caa089
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2025-12-02 22:25:15 -0500

    Time: 514 ms (21.24%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+  ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
+
+
+-- LAG(Population) OVER(PARTITION BY Continent ORDER BY Population DESC) AS value,
