93e682e462a0ccc490d3c4193074748dc8e2d70e
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2025-12-15 15:35:45 -0500

    Time: 62 ms (49.24%) | Memory: 30 MB (48.09%) - LeetSync

--- 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.py
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.py
@@ -5,7 +5,7 @@ class Solution:
         seq = 1
         
         for i in range(1, len(prices)):
-            if prices[i-1] == prices[i] + 1 :
+            if prices[i-1] -1 == prices[i] :
                 seq += 1
             else:
                 seq = 1
