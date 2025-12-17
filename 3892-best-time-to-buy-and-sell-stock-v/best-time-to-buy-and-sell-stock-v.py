e0c7608eff64213248c57bd092161d59bc80aadf
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2025-12-17 16:46:01 -0500

    Time: 2919 ms (33.95%) | Memory: 94 MB (20.89%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.py
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.py
@@ -2,26 +2,17 @@ class Solution:
     def maximumProfit(self, prices: List[int], k: int) -> int:
         n = len(prices)
 
-        @cache
-        def dfs(i, j, state):
-            if j == 0:
-                return 0
-            if i == 0:
-                return (
-                    0 if state == 0 else -prices[0] if state == 1 else prices[0]
-                )
-            p = prices[i]
-            if state == 0:
-                res = max(
-                    dfs(i - 1, j, 0), dfs(i - 1, j, 1) + p, dfs(i - 1, j, 2) - p
-                )
-            elif state == 1:
-                res = max(dfs(i - 1, j, 1), dfs(i - 1, j - 1, 0) - p)
-            else:
-                res = max(dfs(i - 1, j, 2), dfs(i - 1, j - 1, 0) + p)
+        dp = [[[0] * 3 for _ in range(k + 1)] for _ in range(n)]
 
-            return res
+        for j in range(1, k+1):
+            dp[0][j][1] = -prices[0]
+            dp[0][j][2] = prices[0]
+        
+        for i in range(1,n):
+            for j in range(1,k+1):
+                dp[i][j][0] = max(dp[i - 1][j][0],max(dp[i - 1][j][1] + prices[i], dp[i - 1][j][2] - prices[i]))
+                dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i])
+                dp[i][j][2] = max(dp[i-1][j][2], dp[i-1][j-1][0] + prices[i])
+            
+        return dp[n-1][k][0]
 
-        ans = dfs(n - 1, k, 0)
-        dfs.cache_clear()
-        return ans
