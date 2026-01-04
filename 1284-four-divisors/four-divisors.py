e9c104d1bc5ea573718fa8ae54783256ee13e6ff
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2026-01-04 17:26:21 -0500

    Time: 823 ms (10.81%) | Memory: 13.2 MB (60.81%) - LeetSync

--- 1284-four-divisors/four-divisors.py
+++ 1284-four-divisors/four-divisors.py
@@ -5,24 +5,28 @@ class Solution(object):
         :rtype: int
         """
         result = 0
-        
-        def find_divisors(num):
-            sets=set()
-            for i in range(1, int(num ** 0.5)+1):
-                if num % i == 0:
-                    sets.add(i)
-                    sets.add(num / i)
+        def is_prime(n):
+            if n < 2:
+                return False
+            for i in range(2, int(n ** 0.5)+1):
+                if n % i == 0:
+                    return False
+            return True
+
+        for num in nums:
+            n = round(num ** (1.0/3))
+            if is_prime(n) and n ** 3 == num:
+                result += 1 + n + n*n + num
+                continue
             
-            if len(sets) == 4:
-                return sets
-            else:
-                return None
+            for i in range(2, int(num ** 0.5)+1):
+                if num % i == 0:
+                    j = num // i
+                    if i != j and is_prime(i) and is_prime(j):
+                        result += 1 + i + j + num
+                        
+                    
         
-        for n in nums:
-            div = find_divisors(n)
-            if div:
-                result += sum(div)
-        
-        return result
+        return int(result)
             
 
