f167c0fe626fa9d2ef2be1376768a763e7582cf8
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2026-01-05 21:29:14 -0500

    Time: 64 ms (28.24%) | Memory: 20.8 MB (57.77%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
@@ -1,4 +1,5 @@
 from collections import deque
+from collections import defaultdict
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -11,31 +12,43 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        queue = deque([root])
-        max_sum = float('-inf')
-        max_lev = 1
-        cur_lev = 1
+        #BFS
+        # queue = deque([root])
+        # max_sum = float('-inf')
+        # max_lev = 1
+        # cur_lev = 1
 
-        while queue:
-            cur_nodes = len(queue)
-            cur_sum = 0
+        # while queue:
+        #     cur_nodes = len(queue)
+        #     cur_sum = 0
 
-            for i in range(cur_nodes):
-                nod = queue.popleft()
-                cur_sum += nod.val
+        #     for i in range(cur_nodes):
+        #         nod = queue.popleft()
+        #         cur_sum += nod.val
 
-                if nod.left:
-                    queue.append(nod.left)
-                if nod.right:
-                    queue.append(nod.right)
+        #         if nod.left:
+        #             queue.append(nod.left)
+        #         if nod.right:
+        #             queue.append(nod.right)
                 
-            if cur_sum > max_sum:
-                max_sum = cur_sum
-                max_lev = cur_lev
+        #     if cur_sum > max_sum:
+        #         max_sum = cur_sum
+        #         max_lev = cur_lev
             
-            cur_lev += 1
+        #     cur_lev += 1
                 
 
-        return max_lev
+        # return max_lev
+
+        #recursion
+        levels = defaultdict(int)
+        def sum_val(depth,root):
+            if root:
+                levels[depth] += root.val
+                sum_val(depth+1, root.left)
+                sum_val(depth+1, root.right)
+        
+        sum_val(1,root)
+        return max(levels, key=levels.get)
 
         
