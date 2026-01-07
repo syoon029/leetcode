3bdeac4890c5294c4ee62c1a46db8fd81270f462
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2026-01-07 17:39:36 -0500

    Time: 151 ms (60.87%) | Memory: 85.4 MB (95.65%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.py
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.py
@@ -1,31 +1,33 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def maxProduct(self, root: Optional[TreeNode]) -> int:
+class Solution(object):
+    def maxProduct(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
         sub_set = set()
         max_sum = float('-inf')
-        total = 0
 
         def traverse(node):
-            nonlocal max_sum, total
             if not node:
                 return 0
 
             left_subtree = traverse(node.left)
             right_subtree = traverse(node.right)
             sub_tree = node.val + left_subtree + right_subtree
-            max_sum = max(max_sum, (total-sub_tree) * sub_tree)
+            sub_set.add(sub_tree)
 
             return sub_tree
         
         total = traverse(root)
-        traverse(root)
-        return max_sum%(10**9 + 7)
+        print(total)
         
-       # for sub_sum in sub_set:
-        #
-        
+        for sub_sum in sub_set:
+            max_sum = max(max_sum, (total - sub_sum) * sub_sum)
+            
+        return max_sum  % (10**9+7)
