7345d8e25e19e36573f35b8f6f385ee33a002359
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2026-01-11 13:37:05 -0500

    Time: 2126 ms (5.10%) | Memory: 18.1 MB (73.76%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        max_a = 0
+
+        dp = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == "0":
+                    continue
+                width = dp[i][j] = dp[i][j - 1] + 1 if j else 1
+
+                for k in range(i, -1, -1):
+                    width = min(width, dp[k][j])
+                    max_a = max(max_a, width * (i - k + 1))
+
+        return max_a
