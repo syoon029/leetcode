6ad610f660c51596d3a31d494896194ae1834493
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2026-01-12 20:23:25 -0500

    Time: 1550 ms (14.51%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,11 +1,3 @@
-# Write your MySQL query statement below
-with full as (select s.name as sname, s.sales_id, o.com_id, c.name as cname
-from SalesPerson s
-LEFT JOIN Orders o on s.sales_id = o.sales_id
-LEFT JOIN Company c on c.com_id = o.com_id)
-
-SELECT distinct sname as name
-from full
-where sales_id not in (select sales_id from full where cname = 'RED')
-
-
+SELECT name
+from SalesPerson
+Where sales_id not in (select sales_id from Company c RIGHT JOIN Orders o on c.com_id = o.com_id where c.name = 'RED' )
