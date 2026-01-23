49ee3b97269ead610c2b29736f62c99fa5be6825
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2026-01-22 19:44:43 -0500

    Time: 7 ms (95.51%) | Memory: 19.5 MB (8.04%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.py
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.py
@@ -1,27 +1,27 @@
 class Solution:
     def minimumPairRemoval(self, nums: List[int]) -> int:
-        count = 0
 
+        count =0 
         while len(nums) > 1:
-            isAscending = True
+            isAsc = True
             minSum = float("inf")
-            targetIndex = -1
+            idx =-1
+            
+            for i in range(len(nums)-1):
+                pair = nums[i] + nums[i+1]
 
-            for i in range(len(nums) - 1):
-                pair_sum = nums[i] + nums[i + 1]
-
-                if nums[i] > nums[i + 1]:
-                    isAscending = False
-
-                if pair_sum < minSum:
-                    minSum = pair_sum
-                    targetIndex = i
-
-            if isAscending:
+                if nums[i] > nums[i+1]:
+                    isAsc = False
+                
+                if pair < minSum:
+                    minSum = pair
+                    idx = i
+                
+            if isAsc:
                 break
 
-            count += 1
-            nums[targetIndex] = minSum
-            nums.pop(targetIndex + 1)
+            count +=1
+            nums[idx] = minSum
+            nums.pop(idx + 1)
 
-        return count
+        return count        
