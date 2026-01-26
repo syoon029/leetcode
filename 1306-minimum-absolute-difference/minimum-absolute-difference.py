67a801f75c69e17e55c8b073c647fa8611893bfd
Seohee Yoon <159593572+syoon029@users.noreply.github.com>
2026-01-26 18:31:45 -0500

    Time: 49 ms (69.77%) | Memory: 31.6 MB (17.04%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -8,13 +8,15 @@ class Solution:
         min_diff = float('inf')
 
         for i in range(len(arr)-1):
-            min_diff = min(min_diff,arr[i+1] - arr[i])
-            print(min_diff)
-        
-        for i in range(len(arr)-1):
-            if arr[i+1] - arr[i] == min_diff:
-                result.append([arr[i],arr[i+1]])
-        
-        
+            curr_diff = arr[i+1] - arr[i]
+
+            if curr_diff == min_diff:
+                result.append([arr[i], arr[i+1]])
+            else:
+                if curr_diff < min_diff:
+                    result = [[arr[i], arr[i+1]]]
+                    min_diff = curr_diff
+                    
+                
         return result
 
